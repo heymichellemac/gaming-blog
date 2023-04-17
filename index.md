2bf40321278109066944e4fae4b4116e836491da
Michelle Mac <heymichellemac@gmail.com>
2023-04-17 20:00:45 +0100

    Initial commit

---
+++ index.md
@@ -0,0 +1,8 @@
+---
+layout: default
+title: 'Tailwind CSS!'
+---
+
+# Hello World
+
+Lorem ipsum...
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+   content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.html',
+    './*.html',
+    './_site/**/*.html',
+    './_site/index.html',
+    'index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+
+}
